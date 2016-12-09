b08c2ee31249a14c1477746f5c17a1bcef7363f6
Alekzander <egalvi@mail.ru>
2016-12-09 12:58:51 +0300

    Simple example with H2

---
+++ SimpleHibernateExample/src/main/java/ru/egalvi/hibernatetutorial/simpleexample/Employee.java
@@ -0,0 +1,50 @@
+package ru.egalvi.hibernatetutorial.simpleexample;
+
+/**
+ *
+ */
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee() {}
+    public Employee(String fname, String lname, int salary) {
+        this.firstName = fname;
+        this.lastName = lname;
+        this.salary = salary;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId( int id ) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName( String first_name ) {
+        this.firstName = first_name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName( String last_name ) {
+        this.lastName = last_name;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary( int salary ) {
+        this.salary = salary;
+    }
+}
+
+/*create table EMPLOYEE (
+   id INT NOT NULL auto_increment,
+   first_name VARCHAR(20) default NULL,
+   last_name  VARCHAR(20) default NULL,
+   salary     INT  default NULL,
+   PRIMARY KEY (id)
+);*/
