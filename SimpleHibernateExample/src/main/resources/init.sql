9170aa196b3d916b753fd68373ae9a2466a10257
Alekzander <egalvi@mail.ru>
2016-12-09 13:05:04 +0300

    Simple example with H2

---
+++ SimpleHibernateExample/src/main/resources/init.sql
@@ -0,0 +1,11 @@
+create table IF NOT EXISTS EMPLOYEE (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    first_name VARCHAR(20) NULL,
+    last_name VARCHAR(20) NULL,
+    salary INT NULL
+);
+--create table IF NOT EXISTS CERTIFICATE (
+--    id INT NOT NULL auto_increment PRIMARY KEY
+--    certificate_name VARCHAR(30) default NULL,
+--    employee_id INT default NULL
+--);
